4082cd89eae22476ebe3967eac7f2e7fb2c1632e
Yeryun Jung <103527404+YeryunJung@users.noreply.github.com>
2023-01-12 13:43:19 +0900

    Update 중간값찾기.py

--- D1/중간값찾기.py
+++ D1/중간값찾기.py
@@ -1,3 +1,9 @@
-count, a = [input() for _ in range(2)]
-a = list(map(int, a.split()))
-print(count/2)
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+# for test_case in range(1, T + 1):
+    # ///////////////////////////////////////////////////////////////////////////////////
+a = list(map(int, input().split()))
+a.sort()
+b, c = divmod(T, 2)
+result = a[b + c - 1]
+print(result)
