90d0139c496498acac643d80806081a36c95295e
Yeryun Jung <103527404+YeryunJung@users.noreply.github.com>
2023-03-09 23:58:30 +0900

    Create 17136_attach_paper.py

---
+++ algorithm/17136_attach_paper.py
@@ -0,0 +1,40 @@
+# 최소 개수를 붙여야 하니까 가장 큰 색종이부터 부착 시도
+def put_paper(x, y):
+    flag = False
+    for k in range(5, 0, -1):
+        if not flag:
+            break
+        else:
+            flag = False
+        for i in range(x, x + k):
+            if flag:
+                break
+            for j in range(y, y + k):
+                # 색종이가 맞지 않으면 끝낸다 > 더 작은 색종이를 붙인다
+                if board[i][j] != 1:
+                    flag = True
+                    break
+            else:
+                flag = False
+        # 색종이가 맞았으면
+    if not flag:
+        for m in range(x, x + k):
+            for n in range(y, y + k):
+                my_board[m][n] = 1
+        return k
+    # 아무것도 맞는 색종이가 없었으면
+    else:
+        return -1
+
+
+board = [list(map(int, input().split())) for _ in range(10)]
+my_board = [[0] * 10 for _ in range(10)]
+
+result = []
+for i in range(10):
+    for j in range(10):
+        # 색종이 붙일 자리가 있고 이미 붙이지 않았다면
+        if board[i][j] == 1 and my_board[i][j] != 1:
+            result.append(put_paper(i, j))
+
+print(result)
